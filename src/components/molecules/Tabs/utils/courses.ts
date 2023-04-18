4944abe9c8db6946dfa4568a0b218b941e2977e3
Sandra <sandrahernandezjim@gmail.com>
2023-04-18 13:53:27 +0200

    Translate card

--- src/components/molecules/Tabs/utils/courses.ts
+++ src/components/molecules/Tabs/utils/courses.ts
@@ -2,43 +2,43 @@ import ReactIcon from "../../../../images/react.png";
 import JsIcon from "../../../../images/javascript.png";
 export const courses = [
   {
-    title: "Curso React",
-    subtitle: "Navegación con React Router",
+    title: "React Course",
+    subtitle: "Navigation with React Router",
     icon: ReactIcon,
     certification:
       "https://platzi.com/p/Sandra_hernandez/curso/3468-react-router/diploma/detalle/",
   },
   {
-    title: "Curso JS",
-    subtitle: "Consumo de API Rest",
+    title: "JS Course",
+    subtitle: "API Rest",
     icon: JsIcon,
     certification:
       "https://platzi.com/p/Sandra_hernandez/curso/2985-course/diploma/detalle/",
   },
   {
-    title: "Curso React",
-    subtitle: "Patrones de Render y Composición",
+    title: "React Course",
+    subtitle: "Render Patterns and Composition",
     icon: ReactIcon,
     certification:
       "https://platzi.com/p/Sandra_hernandez/curso/2457-react-patrones-render/diploma/detalle/",
   },
   {
-    title: "Curso JS",
-    subtitle: "Asincronismo",
+    title: "JS Course",
+    subtitle: "Asynchronism",
     icon: JsIcon,
     certification:
       "https://platzi.com/p/Sandra_hernandez/curso/1789-asincronismo-js-2019/diploma/detalle/",
   },
   {
-    title: "Curso React",
-    subtitle: "Manejo Profesional del Estado",
+    title: "React Course",
+    subtitle: "Professional State Management",
     icon: ReactIcon,
     certification:
       "https://platzi.com/p/Sandra_hernandez/curso/2597-react-estado/diploma/detalle/",
   },
   {
-    title: "Curso JS",
-    subtitle: "Curso de ECMAScript 6+",
+    title: "JS Course",
+    subtitle: "ECMAScript 6+",
     icon: JsIcon,
     certification:
       "https://platzi.com/p/Sandra_hernandez/curso/1815-ecmascript-6/diploma/detalle/",
